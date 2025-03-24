3315a42dab81f9d5d19f97bcceed52a5e8e4f2f4
raphaprograms <203123602+raphaprograms@users.noreply.github.com>
2025-03-23 20:53:39 -0700

    Refine the code to follow prompt more closely
    The last commit functioned properly but did not require arguments for
    the playRound function. I reevaluated the scope and variable placement
    so that playRound could be run with arguments. Also I organized the code
    to make it easier to follow by eye.

--- script.js
+++ script.js
@@ -2,67 +2,84 @@ playGame();
 
 function playGame() {
 
-/* console.log(humanScore);
-console.log(computerScore); */
-let humanScore = 0; 
-let computerScore = 0; 
+    let humanScore = 0; 
+    let computerScore = 0; 
 
+    let humanSelection 
+    let computerSelection 
 
-console.log(playRound());
-console.log(playRound());
-console.log(playRound());
-console.log(playRound());
-console.log(playRound());
+    console.log(`Your have ${humanScore}`);
+    console.log(`Computer has ${computerScore}`); 
+
+    console.log(playRound(humanSelection, computerSelection));
+    console.log(playRound(humanSelection, computerSelection));
+    console.log(playRound(humanSelection, computerSelection));
+    console.log(playRound(humanSelection, computerSelection));
+    console.log(playRound(humanSelection, computerSelection));
+
+    console.log(`GAME OVER. Refresh to play again. 
+                Your final score is  ${humanScore}
+                Computer final score is ${computerScore}`); 
+
+    function getComputerChoice() {
+        spin = 10 * Math.random()
+        if (spin >= 0 && spin < 3.3) {
+            let computerChoice = "rock"
+            return computerChoice;
+        }
+        else if (spin >= 3.3 && spin < 6.6) {
+            let computerChoice = "paper"
+            return computerChoice;
+        }
+        else if (spin >= 6.6 && spin < 10) {
+            let computerChoice = "scissors"
+            return computerChoice;
+        }
+    }
+
+    function getHumanChoice() {
+        let userChoice = prompt("Choose your weapon 'rock', 'paper', or 'scissors'!", "rock").toLowerCase();
+        return userChoice;
+    }
 
     function playRound(humanChoice, computerChoice) {
 
-        function getComputerChoice() {
-            spin = 10 * Math.random()
-            if (spin >= 0 && spin < 3.3) {
-                return "rock"
-            }
-            else if (spin >= 3.3 && spin < 6.6) {
-                return "paper"
-            }
-            else if (spin >= 6.6 && spin < 10) {
-                return "scissors"
-            }
-        }
-        
-        function getHumanChoice() {
-            let userChoice = prompt("Choose your weapon 'rock', 'paper', or 'scissors'!", "rock/paper/scissors");
-            return userChoice.toLowerCase();
-        }
-
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
+        humanSelection = getHumanChoice();
+        computerSelection = getComputerChoice();
 
         if (humanSelection == computerSelection){
-            return `It is a tie! You have ${humanScore} points, your opponent has ${computerScore} points.`;
+            return `It is a tie! 
+            You have ${humanScore} points, your opponent has ${computerScore} points.`;
         }
         else if (humanSelection == "rock" && computerSelection == "scissors" ) { 
             humanScore += 1;
-            return `You win this round! Rock breaks scissors! You have ${humanScore} points, your opponent has ${computerScore} points.`;
+            return `You win this round! Rock breaks scissors! 
+            You have ${humanScore} points, your opponent has ${computerScore} points.`;
         }
         else if (humanSelection == "rock" && computerSelection == "paper" ) { 
             computerScore += 1;
-            return `You lose this round! Paper covers rock! You have ${humanScore} points, your opponent has ${computerScore} points.`;
+            return `You lose this round! Paper covers rock! 
+            You have ${humanScore} points, your opponent has ${computerScore} points.`;
         }
         else if (humanSelection == "paper" && computerSelection == "rock" ) { 
             humanScore += 1;
-            return `You win this round! Paper covers rock! You have ${humanScore} points, your opponent has ${computerScore} points.`;
+            return `You win this round! Paper covers rock! 
+            You have ${humanScore} points, your opponent has ${computerScore} points.`;
         }
         else if (humanSelection == "paper" && computerSelection == "scissors" ) { 
             computerScore += 1;
-            return `You lose this round! Scissors cuts paper! You have ${humanScore} points, your opponent has ${computerScore} points.`;
+            return `You lose this round! Scissors cuts paper! 
+            You have ${humanScore} points, your opponent has ${computerScore} points.`;
         }
         else if (humanSelection == "scissors" && computerSelection == "paper" ) { 
             humanScore += 1;
-            return `You win this round! Scissors cuts paper! You have ${humanScore} points, your opponent has ${computerScore} points.`;
+            return `You win this round! Scissors cuts paper! 
+            You have ${humanScore} points, your opponent has ${computerScore} points.`;
         }
         else if (humanSelection == "scissors" && computerSelection == "rock" ) {
             computerScore += 1; 
-            return `You lose this round! Rock breaks scissors! You have ${humanScore} points, your opponent has ${computerScore} points.`;
+            return `You lose this round! Rock breaks scissors! 
+            You have ${humanScore} points, your opponent has ${computerScore} points.`;
         }
     }
 }
